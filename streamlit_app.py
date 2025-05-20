c5297c4e6805a54da13c485f6120e70fc64c207c
kayis354 <kayisabdulaziz13@gmail.com>
2025-05-20 19:49:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("  🎈  Reza Madridista  ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 st.image(".17477411463096966609468434841163.jpg", width=200) 
