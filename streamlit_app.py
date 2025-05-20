50887425bddeac035672c951675c6e941f87f96b
kayis354 <kayisabdulaziz13@gmail.com>
2025-05-20 19:56:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("  🎈  Reza Madridista  ")
 st.write(
     "Reza anak sekolah SMAN 20 BANDUNG  .")
-st.image(".17477411463096966609468434841163.jpg", width=200) 
+st.image("17477384579282276395910537665755.jpg", width=200) 
