ad70bb691a5861dc06343a057e281f70870b0496
kayis354 <kayisabdulaziz13@gmail.com>
2025-05-27 07:49:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,16 @@ st.title("  🎈  Reza Madridista  ")
 st.write(
     "Reza anak sekolah SMAN 20 BANDUNG  .")
 st.image("17477384579282276395910537665755.jpg",width=200) 
+
+
+
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
+
+if (angka % 2) ==0:
+ st.write(f"{angka} adalah Bilangan Genap")
+else:
+ st.write(f"{angka}adalah Bilangan Ganjil")
+    
